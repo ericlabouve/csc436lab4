f993ca378aed0bcfb54698a30a46ee7c2a815bec
Eric LaBouve <elabouve@csc235x02.ad.calpoly.edu>
2019-01-28 13:38:01 -0800

    Initial Commit

---
+++ lab4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab4
+//
+//  Created by Eric LaBouve on 1/28/19.
+//  Copyright © 2019 Eric LaBouve. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
